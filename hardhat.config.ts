a7aa224376245706af650221fa7cf74d3959bf0e
0xhardman <1142783160@qq.com>
2024-07-15 02:28:44 +0800

    change network op sepolia and remove rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,19 +21,47 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    rinkeby: {
-      url: process.env.API_URL,
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    // rinkeby: {
+    //   url: process.env.API_URL,
+    //   accounts:
+    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
     mainnet: {
-      url: process.env.API_URL,
+      chainId: 1,
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    OPSepolia: {
+      chainId: 11155420,
+      url: `https://optimism-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      OPSepolia: process.env.OP_SEPOLIA_ETHERSCAN_API_KEY as string,
+      mainnet: process.env.MAINNET_ETHERSCAN_API_KEY as string,
+    },
+    customChains: [
+      {
+        network: 'OPSepolia',
+        chainId: 11155420,
+        urls: {
+          apiURL: 'https://api-sepolia-optimistic.etherscan.io/api',
+          browserURL: 'https://sepolia-optimism.etherscan.io',
+        },
+      },
+      {
+        network: 'mainnet',
+        chainId: 1,
+        urls: {
+          apiURL: 'https://api.etherscan.io/api',
+          browserURL: 'https://etherscan.io',
+        },
+      }
+    ],
   },
 };
 
